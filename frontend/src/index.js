5afea6c92ab26d1c6b0de6584eae78dbd013ba2b
elie-atia <elie-atia@hotmail.fr>
2023-05-03 01:25:05 +0300

    feat: add redux-persit to the store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Store from './state/store';
+import {store ,persistor } from './state/store';
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={Store}>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- frontend/src/state/store.js
+++ frontend/src/state/store.js
@@ -1,19 +1,32 @@
-import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
+import { configureStore, applyMiddleware, combineReducers } from '@reduxjs/toolkit';
 import marketReducer from './marketSlice';
 import tradeReducer from './tradeSlice';
 import logger from 'redux-logger'
 import authReducer from './authSlice';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  market: marketReducer,
+  trade: tradeReducer,
+  auth: authReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    market: marketReducer,
-    trade: tradeReducer,
-    auth: authReducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     process.env.NODE_ENV !== 'production'
       ? getDefaultMiddleware().concat(logger)
       : getDefaultMiddleware(),
-},
-);
-export default store  
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
